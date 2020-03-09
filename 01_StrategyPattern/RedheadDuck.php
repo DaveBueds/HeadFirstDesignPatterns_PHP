fdef665ce7c16d5cb7c34f846a640da5c473054d
davebueds <david@inuits.eu>
2020-03-09 14:11:01 +0100

    Redhead duck implementation

--- 01_StrategyPattern/RedheadDuck.php
+++ 01_StrategyPattern/RedheadDuck.php
@@ -2,7 +2,21 @@
 declare(strict_types=1);
 
 
-class RedheadDuck
+class RedheadDuck extends Duck
 {
+    public function __construct()
+    {
+        $this->flyBehavior = new FlyWithWings();
+        $this->quackBehavior = new Quack();
+    }
 
+    public function swim()
+    {
+        // TODO: Implement swim() method.
+    }
+
+    public function display()
+    {
+        echo 'Im a redhead duck.';
+    }
 }
