2ce1dde8a064462488739478a0b416de16b86cb9
davebueds <david@inuits.eu>
2020-03-09 14:22:50 +0100

    Add test of simUDuck

--- 01_StrategyPattern/index.php
+++ 01_StrategyPattern/index.php
@@ -1,2 +1,14 @@
 <?php
 
+require_once './vendor/autoload.php';
+
+$duck = new MallardDuck();
+$duck->display();
+$duck->performFly();
+echo '\n';
+
+$duck->setFlyBehavior(new FlyNoWay());
+$duck->display();
+$duck->performFly();
+$duck->performQuack();
+echo '\n';
