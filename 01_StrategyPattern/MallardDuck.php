4df918392846cd76dfb1035810c60f7a17feaf69
davebueds <david@inuits.eu>
2020-03-09 14:10:09 +0100

    Mallard duck implementation

--- 01_StrategyPattern/MallardDuck.php
+++ 01_StrategyPattern/MallardDuck.php
@@ -2,7 +2,21 @@
 declare(strict_types=1);
 
 
-class MallardDuck
+class MallardDuck extends Duck
 {
+    public function __construct()
+    {
+        $this->flyBehavior = new FlyWithWings();
+        $this->quackBehavior = new Quack();
+    }
 
+    public function swim()
+    {
+        // TODO: Implement swim() method.
+    }
+
+    public function display()
+    {
+        echo 'Im a Mallard duck.';
+    }
 }
