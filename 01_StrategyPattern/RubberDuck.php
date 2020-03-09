69c4afde6490694e9e8cbbecb3d524034850d275
davebueds <david@inuits.eu>
2020-03-09 14:11:56 +0100

    Rubber duck implementation

--- 01_StrategyPattern/RubberDuck.php
+++ 01_StrategyPattern/RubberDuck.php
@@ -2,7 +2,21 @@
 declare(strict_types=1);
 
 
-class RubberDuck
+class RubberDuck extends Duck
 {
+    public function __construct()
+    {
+        $this->flyBehavior = new FlyNoWay();
+        $this->quackBehavior = new Squeak();
+    }
 
+    public function swim()
+    {
+        // TODO: Implement swim() method.
+    }
+
+    public function display()
+    {
+        echo 'Im a rubber duck.';
+    }
 }
